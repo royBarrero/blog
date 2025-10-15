4c147802da9e56f8fbf9f5e5f0bde0d95f5d83f2
royBarrero <wigroy.barrero@gmail.com>
2025-10-15 15:40:00 -0400

    añadi un nuevo controlador para la pagina principal fr bienvenida

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[HomeController::class, 'index'] );
+Route::get('/posts',function(){
+return  "aqui se mostraran los posts"; 
+});
+Route::get('/posts/create',function(){
+    return "aqui se mostrara el formulario para crear un post";
+});
+Route::get('/posts/{post}',function($post){
+    return "aqui se mostrara el post {$post}";
 });
