7dd69ab4f97d74edff28f0ab72c27255a63e83c2
royBarrero <wigroy.barrero@gmail.com>
2025-10-21 16:56:47 -0400

    agregue nuevo texto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11</title>
+
+     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
-    <h1>bienvenido ala pagina principal</h1>
+    <div class ="max-w-4xl mx-auto px-4">
+        <h1>bienvenido ala pagina principal melisssa</h1>
+    </div>
 </body>
 </html>
